d441a4d4f86ce91f96218096a30168350382d9ac
Ramanand Patil <RamanandPatil@users.noreply.github.com>
2017-07-20 23:11:03 +0530

    Simple GUi Template
    Started Simple GUI learning and, Templatized steps to write any GUI program, using simplegui.

---
+++ Lessons/Week 2a - Interactive Applications in Python/04_examples-simplegui-1.py
@@ -0,0 +1,19 @@
+# http://www.codeskulptor.org/#examples-simplegui-1.py
+
+# SimpleGUI program template
+
+# Import the module
+import simplegui
+
+# Define global variables (program state)
+
+# Define "helper" functions
+
+# Define event handler functions
+
+# Create a frame
+
+# Register event handlers
+
+# Start frame and timers
+
